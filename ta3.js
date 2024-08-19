98b530da169bd900279cc337aef99da10263014e
Ivan-Lomando <ilomandofernandez@gmail.com>
2024-08-19 18:52:08 -0300

    oldest

--- ta3.js
+++ ta3.js
@@ -1,5 +1,11 @@
 ///ejercicio 2
 
 let findTheOldest = (personas) =>{
-    let resultado = 0;
+    let resultado = 0
+    for(i = 0; i <= personas.lenght; i++){
+        if(personas[i] > resultado ){
+            resultado = personas[i]
+        }
+    }
+    return resultado
 }
